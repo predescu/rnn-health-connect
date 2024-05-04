3c417dbdfeeac3ef927fe58f7e0092e7087c8072
Constantin Predescu <constantin.predescu@thefans.io>
2024-05-04 20:14:43 +0100

    feat: rnn

--- android/app/src/main/java/com/rnnhealthconnect/MainApplication.kt
+++ android/app/src/main/java/com/rnnhealthconnect/MainApplication.kt
@@ -1,45 +1,30 @@
 package com.rnnhealthconnect
 
-import android.app.Application
 import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
-import com.facebook.soloader.SoLoader
 
-class MainApplication : Application(), ReactApplication {
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+class MainApplication : NavigationApplication() {
+
+    private val mReactNativeHost: NavigationReactNativeHost =
+            object : NavigationReactNativeHost(this) {
+                override fun getPackages(): List<ReactPackage> =
+                        PackageList(this).packages.apply {
+                            // Packages that cannot be autolinked yet can be added manually here, for example:
+                            // add(MyReactNativePackage())
+                        }
+
+                override fun getJSMainModuleName(): String = "index"
+
+                override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+                override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+                override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
             }
 
-        override fun getJSMainModuleName(): String = "index"
+    override val reactNativeHost: NavigationReactNativeHost
+        get() = mReactNativeHost
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
 }
