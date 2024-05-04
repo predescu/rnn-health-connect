8db5b5fac80383ecb5a10615d536fa8fbf68590d
Constantin Predescu <constantin.predescu@thefans.io>
2024-05-04 20:40:32 +0100

    feat: health connect

--- android/app/src/main/java/com/rnnhealthconnect/MainActivity.kt
+++ android/app/src/main/java/com/rnnhealthconnect/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.rnnhealthconnect
 
+import android.os.Bundle
 import com.reactnativenavigation.NavigationActivity
+import dev.matinzd.healthconnect.permissions.HealthConnectPermissionDelegate
+
 class MainActivity: NavigationActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        // In order to handle permission contract results, we need to set the permission delegate.
+//        HealthConnectPermissionDelegate.setPermissionDelegate(this)
+//    }
+
 }
